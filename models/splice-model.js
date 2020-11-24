15e8786917e13d8f2e0457c6297a0c000bc6cb65
Michael Famurewa <myfamurewa@gmail.com>
2020-11-24 17:23:06 -0500

    hit a roadblock with ffprobe errors

--- models/splice-model.js
+++ models/splice-model.js
@@ -1,20 +1,23 @@
-const concat = require('ffmpeg-concat')
+let videoStitch = require('video-stitch')
+
+let videoConcat = videoStitch.concat;
 
 module.exports = {
     stitch
 }
 
-function stitch(vid1, vid2, filename = ""){
-     let result = concat({
-        output: filename !== ""? `${filename}.mp4` : 'newvid.mp4',
-        videos: [
-            `${vid1.name}`,
-            `${vid2.name}`
-        ],
-        transition: {
-            name: 'circleOpen',
-            duration: 1000
-        }
+function stitch(vid1, vid2, filename){
+    videoConcat({
+        silent: false,
+        overwrite: false
     })
-    return result
+    .clips([
+        {"fileName": vid1.name},
+        {"fileName": vid2.name}
+    ])
+    .output(filename? `${filename}` : 'myNewVideo')
+    .concat()
+    .then((outputFileName) => {
+        return outputFileName
+    })
 }
